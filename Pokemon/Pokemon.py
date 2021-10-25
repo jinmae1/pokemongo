9dde0a1e37cf3ef81b70000e5e1baa1ee05a8beb
Jinmae Jin <jinmae1@gmail.com>
2021-10-26 03:53:57 +0900

    Check data redundancy on insertion

--- Pokemon/Pokemon.py
+++ Pokemon/Pokemon.py
@@ -11,17 +11,24 @@ if __name__ == '__main__':
         host = json_data['host']
         port = json_data['port']
 
-
         client = pymongo.MongoClient(f'mongodb://{host}:{port}/')
-        print(client.list_database_names());
+        # print(client.list_database_names());
         db = client['pokeapi']
         collection = db['pokemon']
 
+        cnt = 0
         for i in range(151):
             res = requests.get(url + str(i + 1))
             if res.status_code == 200:
                 res_dict = res.json()
+                q = collection.find_one({'id' :  i + 1}, {'id': 1, 'name': 1})
+                if q:
+                    print(f'{q["id"]}. {q["name"]} already exists. Skipping')
+                    continue
                 x = collection.insert_one(res_dict)
-                print(f'Inserted {str(i + 1)}')
+                print(f'{res_dict["id"]}. {res_dict["name"]} inserted')
+                cnt += 1
+            else:
+                print(f'status code: {res.status_code} for {url + str(i + 1)}')
     
-        print('Insertion finished')
+        print(f'==== Total of {cnt} documents have been inserted ====')
