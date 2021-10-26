3463b29dc0d1bb2288f95fc34fb82c734e6a1f5e
Jinmae Jin <jinmae1@gmail.com>
2021-10-26 15:37:26 +0900

    Check duplicates by adding index. Handle exceptions
    - Use dotenv

--- Pokemon/Pokemon.py
+++ Pokemon/Pokemon.py
@@ -1,34 +1,43 @@
 import pymongo
-import json
+import dotenv
 import requests
 
 if __name__ == '__main__':
     url = 'https://pokeapi.co/api/v2/pokemon/'
-    with open('./mongodb.json') as json_file:
-        json_data = json.load(json_file)
 
-        print(json_data)
-        host = json_data['host']
-        port = json_data['port']
+    try:
+        env = dotenv.dotenv_values('.env')
+        HOST = env['host']
+        PORT = env['port']
+    except KeyError as ke:
+        print(f'KeyError: {ke} does not exist')
+        exit()
+    except FileNotFoundError:
+        print('.env file does not exist')
+        exit()
 
-        client = pymongo.MongoClient(f'mongodb://{host}:{port}/')
-        # print(client.list_database_names());
-        db = client['pokeapi']
-        collection = db['pokemon']
+    client = pymongo.MongoClient(f'mongodb://{HOST}:{PORT}/')
+    db = client['pokeapi']
+    collection = db['pokemon']
+    # db.pokemon.createIndex( { id: 1 }, { unique: true } )
+    collection.create_index('id', unique=True)
 
-        cnt = 0
-        for i in range(151):
-            res = requests.get(url + str(i + 1))
-            if res.status_code == 200:
-                res_dict = res.json()
-                q = collection.find_one({'id' :  i + 1}, {'id': 1, 'name': 1})
-                if q:
-                    print(f'{q["id"]}. {q["name"]} already exists. Skipping')
-                    continue
+    cnt = 0
+    range_end = 151
+    for i in range(0, range_end):
+        res = requests.get(url + str(i + 1))
+        if res.status_code == 200:
+            res_dict = res.json()
+            try:
                 x = collection.insert_one(res_dict)
                 print(f'{res_dict["id"]}. {res_dict["name"]} inserted')
                 cnt += 1
-            else:
-                print(f'status code: {res.status_code} for {url + str(i + 1)}')
-    
-        print(f'==== Total of {cnt} documents have been inserted ====')
+            except pymongo.errors.DuplicateKeyError:
+                print(f'{res_dict["id"]}. {res_dict["name"]} already exists')
+            except Exception as e:
+                print(e)
+        else:
+            print(f'status code: {res.status_code} for {url + str(i + 1)}')
+
+    print(f'==== Total of {cnt} documents have been inserted ====')
+    print(f'And {range_end - cnt} failed')
